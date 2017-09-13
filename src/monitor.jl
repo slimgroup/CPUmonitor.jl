c335bef923edaa49cd1b35e09f2810c2e74eb394
Keegan Lensink <keeganlensink@gmail.com>
2017-09-12 17:31:28 -0700

    remove log in pkg dir

--- src/monitor.jl
+++ src/monitor.jl
@@ -16,11 +16,10 @@ function monitor(username::String;
                     delay::Number = 0.05,
                     iterations::Int = 10,
                     command::String = "julia",
-                    logfile::String = Pkg.dir("CPUmonitor")*"/log/$(now()).txt",
+                    logfile::String = tempname()*".CPUmonitor.log.$(now()).txt",
                     verbose::Bool = true)
-
+    verbose && println("Log: $logfile")
     logfile = track(username, delay, iterations, command, logfile, verbose)
-
     stat = parselog(logfile, verbose)
 end
 
