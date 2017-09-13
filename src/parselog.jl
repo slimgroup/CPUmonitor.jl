af5350f4881c4d6f66796c0ee835d79e73b51d4b
Keegan Lensink <keeganlensink@gmail.com>
2017-09-12 17:42:44 -0700

    organize

--- src/parselog.jl
+++ src/parselog.jl
@@ -9,18 +9,13 @@ function parselog(logfile::String, verbose::Bool)
     # Find unique process IDs
     pids = unique(log[:,1])
     count = 1
-
     stat = Array{CPUStat,1}(length(pids))
     
     # Collect stats from each PID into an instance of CPUStat
     for pid in pids
-
         inds = find(x -> x == pid, log[:,1])
-
         stat[count] = CPUStat(pid, log[inds, 9], log[inds, 10])
-        
         count += 1
-
     end
 
     verbose && println("CPUmonitor: End Parsing Log...")
